d50be6bf8d98bd7610291cbce2676ef792a4c4cf
Keisuke Oshima <koisland@sarlacc.pmacs.upenn.edu>
2025-08-04 13:51:46 -0400

    Add merge subcommand and move extract monomers to extract subcommand.

--- src/cli.rs
+++ src/cli.rs
@@ -1,28 +1,58 @@
 use std::{f32, path::PathBuf};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 /// Script to take `srf` and `trf` output and produce a bed file with only regions corresponding monomers of a given periodicity.
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 pub struct Cli {
-    #[arg(short, long)]
-    /// PAF file of alignment of assembly as query and `srf` enlonged motifs as target.
-    /// Requires `cg` extended cigar string. With `minimap2`, use `--eqx`.
-    pub paf: PathBuf,
-    /// `trf` monomers TSV file on `srf` monomers with columns:
-    /// `chrom (query), motif (target), st, end, period, copyNum, fracMatch, fracGap, score, entropy, pattern`
-    #[arg(short, long)]
-    pub monomers: PathBuf,
-    /// Output BED9 file with columns:
-    /// `chrom, st, end, comma-delimited_monomers, 0, strand, st, end, '0,0,0'`
-    #[arg(short, long)]
-    pub outfile: Option<PathBuf>,
-    /// Monomer size in base pairs to search for.
-    #[arg(short, long, default_values_t = [170, 340, 42])]
-    pub sizes: Vec<u32>,
-    /// Percent difference in monomer period allowed.
-    /// ex. `0.02` results in valid periods for `170`: `167 < 170 < 173`
-    #[arg(short, long, default_value_t = 0.02)]
-    pub diff: f32,
+    #[command(subcommand)]
+    pub command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    Extract {
+        #[arg(short, long)]
+        /// PAF file of alignment of assembly as query and `srf` enlonged motifs as target.
+        /// Requires `cg` extended cigar string. With `minimap2`, use `--eqx`.
+        paf: PathBuf,
+        /// `trf` monomers TSV file on `srf` monomers with columns:
+        /// `chrom (query), motif (target), st, end, period, copyNum, fracMatch, fracGap, score, entropy, pattern`
+        #[arg(short, long)]
+        monomers: PathBuf,
+        /// Output BED9 file with columns:
+        /// `chrom, st, end, comma-delimited_monomers, 0, strand, st, end, '0,0,0'`
+        #[arg(short, long)]
+        outfile: Option<PathBuf>,
+        /// Monomer size in base pairs to search for.
+        #[arg(short, long, default_values_t = [170, 340, 42])]
+        sizes: Vec<u32>,
+        /// Percent difference in monomer period allowed.
+        /// ex. `0.02` results in valid periods for `170`: `167 < 170 < 173`
+        #[arg(short, long, default_value_t = 0.02)]
+        diff: f32,
+    },
+    Merge {
+        /// Bed file from extract command.
+        #[arg(short, long)]
+        bed: PathBuf,
+        /// Output BED9 file with columns:
+        /// `chrom, st, end, comma-delimited_monomers, 0, strand, st, end, '0,0,0'`
+        #[arg(short, long)]
+        outfile: Option<PathBuf>,
+        /// Distance to merge.
+        #[arg(short, long, default_value_t = 1_000_000)]
+        dst: u32,
+        /// Minimum length.
+        #[arg(short, long, default_value_t = 30_000)]
+        min_len: u32,
+        /// Required monomers in merged blocks. Merges iff one of these monomer periods is in block.
+        /// Also filters out monomers not with this period.
+        #[arg(short, long, default_values_t = [170, 340])]
+        sizes: Vec<u32>,
+        /// Difference in required monomer size.
+        #[arg(long, default_value_t = 0.02)]
+        diff: f32,
+    },
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::{
-    collections::HashSet,
+    collections::{HashSet, VecDeque},
+    ffi::OsStr,
     fs::File,
-    io::{BufWriter, Write, stdout},
+    io::{BufRead, BufReader, BufWriter, Write, stdin, stdout},
 };
 
 use clap::Parser;
@@ -14,92 +15,203 @@ mod cigar;
 mod cli;
 mod io;
 
-use crate::{cigar::get_aligned_paired_itvs, cli::Cli, io::read_trf_monomers};
+use crate::{
+    cigar::get_aligned_paired_itvs,
+    cli::{Cli, Command},
+    io::read_trf_monomers,
+};
 
 fn main() -> eyre::Result<()> {
     let cli = Cli::parse();
-    eprintln!("Using config:\n{:#?}", &cli);
+    eprintln!("Running command:\n{:#?}", &cli.command);
 
-    let monomers = read_trf_monomers(cli.monomers)?;
-    let reader = Reader::from_path(cli.paf)?;
-    let monomer_periods: HashSet<u32> = HashSet::from_iter(cli.sizes);
-    let perc_diff = cli.diff;
-    let mut writer = if let Some(outfile) = cli.outfile {
-        Box::new(BufWriter::new(File::create(outfile)?)) as Box<dyn Write>
-    } else {
-        Box::new(BufWriter::new(stdout())) as Box<dyn Write>
-    };
-
-    let Some(min_monomer_period) = monomer_periods.iter().min().cloned() else {
-        bail!("No monomer periods provided.")
-    };
-
-    // Inteval tree of allowed period ranges.
-    let monomer_period_range: Lapper<u32, ()> = Lapper::new(
-        monomer_periods
-            .iter()
-            .map(|period| {
-                let allowed_diff = *period as f32 * perc_diff;
-                let lower_bound = *period as f32 - allowed_diff;
-                let upper_bound = *period as f32 + allowed_diff;
-                Interval {
-                    start: lower_bound as u32,
-                    stop: upper_bound as u32 + 1,
-                    val: (),
-                }
-            })
-            .collect(),
-    );
-    eprintln!(
-        "Using monomer periodicity range:\n{:#?}",
-        monomer_period_range.intervals
-    );
-
-    for rec in reader
-        .into_records()
-        .flatten()
-        .filter(|rec| rec.tp().eq(&Some(&'P')))
-        .sorted_by(|a, b| a.query_start().cmp(&b.query_start()))
-    {
-        let paired_itvs = get_aligned_paired_itvs(&rec, min_monomer_period)?;
-
-        let Some(target_chrom_monomers) = monomers.get(rec.query_name()) else {
-            continue;
-        };
-        for (q_itv, t_itv) in paired_itvs {
-            let Some(target_tr_chrom_monomers) = target_chrom_monomers.get(rec.target_name())
-            else {
-                continue;
+    match cli.command {
+        Command::Extract {
+            paf,
+            monomers,
+            outfile,
+            sizes,
+            diff,
+        } => {
+            let monomers = read_trf_monomers(monomers)?;
+            let reader = Reader::from_path(paf)?;
+            let monomer_periods: HashSet<u32> = HashSet::from_iter(sizes);
+            let mut writer = if let Some(outfile) = outfile {
+                Box::new(BufWriter::new(File::create(outfile)?)) as Box<dyn Write>
+            } else {
+                Box::new(BufWriter::new(stdout())) as Box<dyn Write>
             };
-            let ovl = target_tr_chrom_monomers
-                .find(t_itv.start, t_itv.stop)
-                .collect_vec();
 
-            if ovl.is_empty() {
-                continue;
+            let Some(min_monomer_period) = monomer_periods.iter().min().cloned() else {
+                bail!("No monomer periods provided.")
+            };
+
+            // Inteval tree of allowed period ranges.
+            let monomer_period_range: Lapper<u32, ()> = Lapper::new(
+                monomer_periods
+                    .iter()
+                    .map(|period| {
+                        let allowed_diff = *period as f32 * diff;
+                        let lower_bound = *period as f32 - allowed_diff;
+                        let upper_bound = *period as f32 + allowed_diff;
+                        Interval {
+                            start: lower_bound as u32,
+                            stop: upper_bound as u32 + 1,
+                            val: (),
+                        }
+                    })
+                    .collect(),
+            );
+            eprintln!(
+                "Using monomer periodicity range:\n{:#?}",
+                monomer_period_range.intervals
+            );
+
+            for rec in reader
+                .into_records()
+                .flatten()
+                .filter(|rec| rec.tp().eq(&Some(&'P')))
+                .sorted_by(|a, b| a.query_start().cmp(&b.query_start()))
+            {
+                let paired_itvs = get_aligned_paired_itvs(&rec, min_monomer_period)?;
+
+                let Some(target_chrom_monomers) = monomers.get(rec.query_name()) else {
+                    continue;
+                };
+                for (q_itv, t_itv) in paired_itvs {
+                    let Some(target_tr_chrom_monomers) =
+                        target_chrom_monomers.get(rec.target_name())
+                    else {
+                        continue;
+                    };
+                    let ovl = target_tr_chrom_monomers
+                        .find(t_itv.start, t_itv.stop)
+                        .collect_vec();
+
+                    if ovl.is_empty() {
+                        continue;
+                    }
+
+                    let monomers = ovl
+                        .iter()
+                        .filter_map(|o| {
+                            (monomer_period_range.count(o.val.trf_period, o.val.trf_period) > 0)
+                                .then_some(&o.val.trf_monomer)
+                        })
+                        .join(",");
+                    if monomers.is_empty() {
+                        continue;
+                    }
+                    writeln!(
+                        &mut writer,
+                        "{}\t{}\t{}\t{}\t0\t{}\t{}\t{}\t0,0,0",
+                        rec.query_name(),
+                        q_itv.start,
+                        q_itv.stop,
+                        monomers,
+                        rec.strand(),
+                        q_itv.start,
+                        q_itv.stop,
+                    )?;
+                }
             }
+        }
+        Command::Merge {
+            bed,
+            outfile,
+            dst,
+            min_len,
+            sizes,
+            diff,
+        } => {
+            let reader = if bed != OsStr::new("-") {
+                Box::new(BufReader::new(File::open(bed)?)) as Box<dyn BufRead>
+            } else {
+                Box::new(BufReader::new(stdin()))
+            };
+            let mut writer = if let Some(outfile) = outfile {
+                Box::new(BufWriter::new(File::create(outfile)?)) as Box<dyn Write>
+            } else {
+                Box::new(BufWriter::new(stdout())) as Box<dyn Write>
+            };
+            let monomer_period_range: Lapper<u32, ()> = Lapper::new(
+                sizes
+                    .iter()
+                    .map(|period| {
+                        let allowed_diff = *period as f32 * diff;
+                        let lower_bound = *period as f32 - allowed_diff;
+                        let upper_bound = *period as f32 + allowed_diff;
+                        Interval {
+                            start: lower_bound as u32,
+                            stop: upper_bound as u32 + 1,
+                            val: (),
+                        }
+                    })
+                    .collect(),
+            );
+            eprintln!(
+                "Using monomer periodicity range:\n{:#?}",
+                monomer_period_range.intervals
+            );
 
-            let monomers = ovl
-                .iter()
-                .filter_map(|o| {
-                    (monomer_period_range.count(o.val.trf_period, o.val.trf_period) > 0)
-                        .then_some(&o.val.trf_monomer)
+            let mut final_intervals: Vec<(String, u32, u32, HashSet<String>)> = vec![];
+            let mut intervals: VecDeque<(String, u32, u32, HashSet<String>)> = reader
+                .lines()
+                .map_while(Result::ok)
+                .map(|line| {
+                    let (chrom, st, end, monomers, _, _, _, _, _) =
+                        line.split('\t').collect_tuple().unwrap();
+                    (
+                        chrom.to_owned(),
+                        st.parse::<u32>().unwrap(),
+                        end.parse::<u32>().unwrap(),
+                        monomers.split(',').map(|m| m.to_owned()).collect(),
+                    )
                 })
-                .join(",");
-            if monomers.is_empty() {
-                continue;
+                .sorted_by(|a, b| (&a.0, a.1).cmp(&(&b.0, b.1)))
+                .collect();
+
+            while let Some(mut itv_1) = intervals.pop_front() {
+                let Some(itv_2) = intervals.pop_front() else {
+                    // Remove anything that isn't in required monomer period range.
+                    itv_1.3.retain(|m| {
+                        monomer_period_range.count(m.len() as u32, m.len() as u32) != 0
+                    });
+
+                    final_intervals.push(itv_1);
+                    break;
+                };
+                let dst_between = itv_2.1.saturating_sub(itv_1.2);
+                if dst_between <= dst && itv_1.0 == itv_2.0 {
+                    intervals.push_front((
+                        itv_1.0,
+                        itv_1.1,
+                        itv_2.2,
+                        itv_1
+                            .3
+                            .union(&itv_2.3)
+                            .cloned()
+                            .collect::<HashSet<String>>(),
+                    ));
+                } else {
+                    let final_itv_len = itv_1.2 - itv_1.1;
+                    itv_1.3.retain(|m| {
+                        monomer_period_range.count(m.len() as u32, m.len() as u32) != 0
+                    });
+
+                    if final_itv_len > min_len && !itv_1.3.is_empty() {
+                        final_intervals.push(itv_1);
+                    }
+                    intervals.push_front(itv_2);
+                }
             }
-            writeln!(
-                &mut writer,
-                "{}\t{}\t{}\t{}\t0\t{}\t{}\t{}\t0,0,0",
-                rec.query_name(),
-                q_itv.start,
-                q_itv.stop,
-                monomers,
-                rec.strand(),
-                q_itv.start,
-                q_itv.stop,
-            )?;
+            for (chrom, st, end, monomers) in final_intervals {
+                let monomers = monomers.iter().join(",");
+                writeln!(
+                    &mut writer,
+                    "{chrom}\t{st}\t{end}\t{monomers}\t0\t.\t{st}\t{end}\t0,0,0"
+                )?;
+            }
         }
     }
 
